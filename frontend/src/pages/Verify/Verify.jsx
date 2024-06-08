1ad9f8ed228a47f704a4f364014b327258d19be4
[Arshu200] <[shaik22arshad@gmail.com]>
2024-06-08 13:45:04 +0530

    first commit

---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
+import './Verify.css';
+
+const Verify = () => {
+    const { url } = useContext(StoreContext);
+    const [searchParams] = useSearchParams();
+    const success = searchParams.get("success");
+    const orderId = searchParams.get("orderId");
+    
+
+    const navigate = useNavigate();
+
+    const verifyPayment = async () => {
+        try {
+            const response = await axios.post(url + "/api/order/verify", { success, orderId });
+            
+            if (response.data.success) {
+                navigate("/myorders");
+            } else {
+                navigate("/");
+            }
+        } catch (error) {
+            console.error("Error verifying payment:", error);
+            navigate("/");
+        }
+    };
+
+    useEffect(() => {
+        verifyPayment();
+    }, []); // Add an empty dependency array to ensure it runs only once
+
+    return (
+        <div className='verify'>
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Verify;
